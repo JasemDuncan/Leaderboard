f8cd47ac1d84fbc8847eecd39165d79de385af06
Jasem Duncan <jasemvalencia@gmail.com>
2021-11-18 16:44:14 -0500

    Implement GET and POST methods using the API

--- src/logicBoard.js
+++ src/logicBoard.js
@@ -1,12 +1,80 @@
-export const loadScoreBoard = (arr) => {
+// GET
+export const loadScoreBoard = async () => {
+  let items;
+  const msjErr = document.getElementById('lblError');
+  msjErr.hidden = true;
+
   const ulScore = document.getElementById('ulScore');
+  ulScore.innerHTML = '';
   ulScore.classList = 'list-group';
-  for (let i = 0; i < arr.length; i += 1) {
-    const lsScore = document.createElement('li');
-    lsScore.classList = 'list-group-item';
-    lsScore.innerText = `${arr[i].name}: ${arr[i].score}`;
-    ulScore.appendChild(lsScore);
+
+  try {
+    const URL = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/7nRAT4myJWoTOfOTMxhR/scores', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    items = await URL.json();
+  } catch (err) {
+    ulScore.innerHTML += `Error in the API${err}`;
+  }
+
+  if (items.result.length) {
+    for (let j = 0; j < items.result.length; j += 1) {
+      const lsScore = document.createElement('li');
+      lsScore.classList = 'list-group-item';
+      lsScore.innerText = `${items.result[j].user}:  ${items.result[j].score}`;
+      ulScore.appendChild(lsScore);
+    }
+  } else {
+    ulScore.innerHTML
+    += `
+      <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+        <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+          <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+        </symbol>
+      </svg>
+
+      <div class="alert alert-danger d-flex align-items-center" role="alert">
+        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+        <div>
+          No items 
+        </div>
+      </div>
+    `;
   }
 };
 
-export default loadScoreBoard;
+// POST
+export const postScores = async () => {
+  const name = document.getElementById('txtName');
+  const score = document.getElementById('txtScore');
+  const msjErr = document.getElementById('lblError');
+
+  if (name.value.length && score.value.length) {
+    try {
+      await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/7nRAT4myJWoTOfOTMxhR/scores', {
+        method: 'POST',
+        body: JSON.stringify(
+          {
+            user: name.value,
+            score: score.value,
+          },
+        ),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      score.value = '';
+      name.value = '';
+    } catch (err) {
+      msjErr.hidden = false;
+      msjErr.innerText += `Error in the API ${err}`;
+    }
+    loadScoreBoard();
+  } else {
+    msjErr.hidden = false;
+    msjErr.innerText += 'Fill name and score';
+  }
+};
